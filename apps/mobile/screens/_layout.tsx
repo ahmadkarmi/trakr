855f0421ccf09d3eaa385ad94fe746771c50d48a
Trakr Dev <dev@trakr.local>
2025-09-18 08:33:15 +0300

    chore(mobile): remove unused screens/_layout.tsx (migrated to React Navigation)

--- apps/mobile/screens/_layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { View } from 'react-native';
-
-// Placeholder: React Navigation is configured in App.js
-export default function RootLayout() {
-  return <View />;
-}
